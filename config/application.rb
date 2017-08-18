d825856d17af338512c36529e7f04c982279f2e6
Alan English & Tyler Fridley & Paul Hammerschmidt <pair+alanenglish.fridl8.motto813@devbootcamp.com>
2017-08-18 11:59:59 -0500

    changed rails settings to reflect api-mode configuration as defined in railsguides

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module ColdBacon
   class Application < Rails::Application
+    # Rails in API mode
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
