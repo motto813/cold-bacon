c99ef2fa1dd153013c6c37971d2ff4cbe7f32964
Alan English & Paul Hammerschmidt <pair+alanenglish.motto813@devbootcamp.com>
2017-08-19 12:52:53 -0500

    actor instance method get top movies finds or creates by movies with validation passing attributes. that method correctly returns top movies. movies for an actor index renders json with passing tests in game management api spec.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,7 @@ class MoviesController < ApplicationController
   def index
     @actor = Actor.find(params[:actor_id])
     @movies = @actor.get_top_movies
+    render json: @movies
   end
 
   def show
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,16 +1,16 @@
 class Actor < ApplicationRecord
   has_many :roles
-  has_many :top_movies, through: :roles
+  has_many :top_movies, through: :roles, source: :movie
   has_many :paths, as: :traceable
 
   validates_presence_of :name, :image_url, :tmdb_id
   validates_uniqueness_of :name, :tmdb_id
 
   def get_top_movies
-    top_movies = known_for_movies(search_api_for_actor)
-    top_movies.each do |movie|
-      top_movie = Movie.find_or_create_by(tmdb_id: movie["id"])
-      Role.find_or_create_by(actor: self, movie: top_movie)
+    movies = known_for_movies(search_api_for_actor)
+    movies.each do |movie|
+      movie = Movie.find_or_create_by(title: movie["title"], tmdb_id: movie["id"], image_url: movie["poster_path"])
+      Role.find_or_create_by(actor: self, movie: movie)
     end
     top_movies
   end
