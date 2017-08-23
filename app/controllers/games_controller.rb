f9b3ff408a81c46641cf7c7d635a894b52bf633a
Paul Hammerschmidt <hammerschmidtpaul@gmail.com>
2017-08-23 10:14:01 -0500

    seeds file has kevin bacon, a few other new actors, a few deleted actors, and all movies commented out. game model setter methods use getter method if starting and ending actor previously set or sample the database. games controller has create_demo route with private methods to set demo starting and ending actors.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,4 +12,34 @@ class GamesController < ApplicationController
       render body: nil, status: 400
     end
   end
+
+  def create_demo
+    @game = Game.new
+    set_demo_starting_actor
+    set_demo_ending_actor
+    if @game.save
+      redirect_to @game
+    else
+      render body: nil, status: 400
+    end
+  end
+
+  private
+  def set_demo_starting_actor
+    demo_starting_actor = Actor.find_or_initialize_by(tmdb_id: 9778)
+    if demo_starting_actor.new_record?
+      demo_starting_actor.assign_attributes(name: "Ice Cube", image_url: "/dzdn1tyWkC4EjlBVKvpAhg5osYA.jpg", popularity: 6.46)
+      demo_starting_actor.save
+    end
+    @game.starting_actor = demo_starting_actor
+  end
+
+  def set_demo_ending_actor
+    demo_ending_actor = Actor.find_or_initialize_by(tmdb_id: 4724)
+    if demo_ending_actor.new_record?
+      demo_ending_actor.assign_attributes(name: "Kevin Bacon", image_url: "/p1uCaOjxSC1xS5TgmD4uloAkbLd.jpg", popularity: 10000)
+      demo_ending_actor.save
+    end
+    @game.ending_actor = demo_ending_actor
+  end
 end
